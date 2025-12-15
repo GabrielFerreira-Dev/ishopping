aa915dd57404b04d2f65988e71c79f83a4cb07e8
Gabriel <gabrieel.ferreira55@gmail.com>
2025-12-15 20:49:08 -0300

    feat(order): message broker with kafka

--- order/src/main/java/com/github/ishopping/order/OrderApplication.java
+++ order/src/main/java/com/github/ishopping/order/OrderApplication.java
@@ -1,10 +1,18 @@
 package com.github.ishopping.order;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class OrderApplication {
+
+	//@Bean
+	public CommandLineRunner commandLineRunner(KafkaTemplate<String, String> template) {
+		return args -> template.send("ishopping.orders-paid", "data", "{ json test }");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
